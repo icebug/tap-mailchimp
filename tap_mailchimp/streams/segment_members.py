72cab2f0619040ca1dcf801e02f81c74843f248c
MariaPantea <mariapantea87@gmail.com>
2021-04-09 18:30:23 +0200

    Update segment_members

--- tap_mailchimp/streams/segment_members.py
+++ tap_mailchimp/streams/segment_members.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from dateutil.parser import parse
 from tap_mailchimp.streams.base import BaseStream
 import singer
 
@@ -24,21 +25,53 @@ class SegmentsMemberStream(BaseStream):
 
     def sync_data(self):
         LOGGER.info("Syncing data for {}".format(self.TABLE))
+        total_lists = 100
+        offset = 0
+        count = 100
 
         # get all lists
-        response = self.client.make_request(path='/lists', method='GET', params={"count": 1000})
-        data = response['lists']
-        lists = list(map(lambda x: x['id'], data))
+        list_ids = []
+        while offset < total_lists:
+            list_params = {
+                "count": count,
+                "offset": offset,
+                "since_date_created": (parse(self.config.get('start_date'))).isoformat(),
+                "sort_field": "date_created",
+                "sort_dir": "ASC",
+                "fields": "lists.id,total_items"
+            }
+            response = self.client.make_request(path='/lists', method='GET', params=list_params)
+            data = response['lists']
+            list_ids += list(map(lambda x: x['id'], data))
+
+            total_lists = response['total_items']
+            offset += count
 
         operations = []
-        for list_id in lists:
+        for list_id in list_ids:
             path = '/lists/{}/segments'.format(list_id)
-            # get all segments
-            response = self.client.make_request(path=path, method='GET', params={"count": 1000})
-            data = response['segments']
-            segments = list(map(lambda x: str(x['id']), data))
 
-            for segment_id in segments:
+            # get all segments
+            total_segments = 100
+            offset = 0
+            count = 1000
+
+            segment_ids = []
+            while offset < total_segments:
+                segment_params = {
+                    "count": count,
+                    "offset": offset,
+                    "fields": "segments.id,total_items"
+                }
+
+                response = self.client.make_request(path=path, method='GET', params=segment_params)
+                data = response['segments']
+                segment_ids += list(map(lambda x: str(x['id']), data))
+
+                total_segments = response['total_items']
+                offset += count
+
+            for segment_id in segment_ids:
                 operations.append(
                     {
                         'method': self.API_METHOD,
@@ -46,11 +79,11 @@ class SegmentsMemberStream(BaseStream):
                         'operation_id': segment_id,
                         'params': {
                             'since': self.get_start_date(self.TABLE).isoformat(),
-                            'exclude_fields': "members._links,members.merge_fields,members.interests"
+                            'exclude_fields': "members._links,members.merge_fields,members.interests,members.location"
                         }
                     }
                 )
-
+        LOGGER.info('Number of segments: {}'.format(len(segment_ids)))
         self.batch_sync_data(operations)
 
     def get_stream_data(self, response, operation_id=None):
